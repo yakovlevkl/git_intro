a425c027cacad4e637f61278991aa5b385618d9f
Яковлев Константин Львович <yakovlev.kl@gazprom-neft.ru>
2023-03-04 21:26:53 +0500

    added func for extract local variables

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
-def test_it():
+import os
+
+from dotenv import load_dotenv
+
+
+def it():
     for i in (1, 2, 3, 4, 5):
         print(i)
 
 
 if __name__ == "__main__":
-    test_it()
+    it()
+    load_dotenv('var.env')
+    key1 = os.environ.get('key1')
+    print(f"{key1=}")
